a999675ab9579f6e6814c27fa75276e678521ea8
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-06-13 00:39:28 +0300

    hotfix user/patch

--- controllers/user.js
+++ controllers/user.js
@@ -64,64 +64,59 @@ const CreateUser = (req, res) => {
 };
 
 const editProfileUser = (req, res) => {
-    const { name, about } = req.body;
-    User.findByIdAndUpdate(req.user._id, { name: name, about: about }, {new: true, runValidators: true, upsert: true})
-      .then ( (user) =>  res.status(200).send({data: user}))
-      .catch ((err) {
-    if (err.name === 'ValidationError') {
-      res
-        .status(400)
-        .send({ message: 'Вы ввели некоректные данные',
-          err: err.message,
-          stack: err.stack});
-    } else if (err.message.includes('Cast to ObjectId failed for value')) {
-      res
-        .status(404)
-        .send({
-          message: 'Запрашиваемый пользователь не найден',
-          err: err.message,
-          stack: err.stack,
-        });
-    } else {
-      res
-        .status(500)
-        .send({
-          message: 'Internal Server Error',
-          err: err.message,
-          stack: err.stack,
-        });
-    }
-  });
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, {new: true,
+    runValidators: true,
+    upsert: true})
+    .then ((user) => res.status(200).send({data: user}))
+    .catch ((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({ message: 'Вы ввели некоректные данные'});
+      } else if (err.message.includes('Cast to ObjectId failed for value')) {
+        res
+          .status(404)
+          .send({
+            message: 'Запрашиваемый пользователь не найден'});
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Internal Server Error',
+            err: err.message,
+            stack: err.stack,
+          });
+      }
+});
 };
 
-const editAvatarUser = async (req, res) => {
-  try {
-    const { avatar } = req.body;
-    const profile = await User.findByIdAndUpdate(req.user._id, { avatar: avatar}, { new: true });
-    res.status(200).send(profile);
-  } catch (err) {
-    if (err.message.includes('validation failed')) {
-      res
-      .status(400)
-      .send({ message: 'Вы ввели некоректные данные' });
-    } else if (err.message.includes('Cast to ObjectId failed for value')) {
-      res
-        .status(404)
-        .send({
-          message: 'Запрашиваемый пользователь не найден',
-          err: err.message,
-          stack: err.stack,
-        });
-    } else {
-      res
-        .status(500)
-        .send({
-          message: 'Internal Server Error',
-          err: err.message,
-          stack: err.stack,
-        });
-    }
-  };
+const editAvatarUser = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, {new: true,
+    runValidators: true,
+    upsert: true})
+    .then ((user) => res.status(200).send({data: user}))
+    .catch ((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({ message: 'Вы ввели некоректные данные'});
+      } else if (err.message.includes('Cast to ObjectId failed for value')) {
+        res
+          .status(404)
+          .send({
+            message: 'Запрашиваемый пользователь не найден'});
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Internal Server Error',
+            err: err.message,
+            stack: err.stack,
+          });
+      }
+});
 };
 
 module.exports = { getUsers, getUsersbyId, CreateUser, editProfileUser, editAvatarUser };
