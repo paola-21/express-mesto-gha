08af1df2fcf6a378c02682d4bf26d066d5060d1e
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-06-26 22:08:55 +0300

    err users

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { getUsers, getUsersbyId, editProfileUser, editAvatarUser, getUser } = require('../controllers/user');
 const auth = require('../middlwares/auth');
 
-router.get('/:id', getUsersbyId);
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().required().length(24).hex(),
+  }),
+}), getUsersbyId);
 
 router.use(auth);
 
@@ -10,8 +14,17 @@ router.get('/me', getUser);
 
 router.get('', getUsers);
 
-router.patch('/me', editProfileUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), editProfileUser);
 
-router.patch('/me/avatar', editAvatarUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/),
+  }),
+}), editAvatarUser);
 
 module.exports = router;
