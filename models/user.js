21b2cc97af071fe0c6b8d3a892e3e19f31fc5ea3
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-06-26 18:23:13 +0300

    err cards

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
   about: {
     type: String,
     required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [3, 'Минимальная длина поля "about" - 3'],
+    minlength: [2, 'Минимальная длина поля "about" - 3'],
     maxlength: [30, 'Максимальная длина поля "about" - 30'],
     default: 'Исследователь',
   },
