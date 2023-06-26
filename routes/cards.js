0ec93b131017d7b20fd44803e5a520e0f002735d
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-06-26 22:52:39 +0300

    err cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,28 @@
 const router = require('express').Router();
 const { getCards, createCards, deleteCardbyId, likeCard, dislikeCard } = require('../controllers/card');
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlwares/auth');
 
 router.use(auth);
 
 router.get('', getCards);
 
-router.post('', createCards);
+router.post('', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/),
+  }),
+}), createCards);
 
 router.delete('/:cardId', deleteCardbyId);
 
 router.put('/:cardId/likes', likeCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().length(24),
+  }),
+}), likeCard);
 
 router.delete('/:cardId/likes', dislikeCard);
 
